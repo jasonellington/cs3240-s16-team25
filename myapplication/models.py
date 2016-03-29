6660099411a3edf57089d07183eaad69c0e3ee59
Luke Botti <lmb2ma@virginia.edu>
2016-03-29 14:18:45 -0400

    Added a boolean field to signify admin status to users.  It is set to false by default

--- myapplication/models.py
+++ myapplication/models.py
@@ -4,6 +4,6 @@ from django.db import models
 class User(models.Model):
     username = models.CharField(max_length=20, unique=True)
     password = models.CharField(max_length=32, unique=False)
-
+    admin = models.BooleanField(default=False)
     def __str__(self):
         return self.username
