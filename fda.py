7bd7bbb570253cf5aebcfd55174be59782c89cb1
Daniel Codos <dbc5ba@virginia.edu>
2016-04-25 11:39:46 -0400

    Updated FDA for file encryption

--- fda.py
+++ fda.py
@@ -1,12 +1,50 @@
+import base64
 import copy
 import json
+import os
+import random
 import urllib.parse
 import urllib.request
 import getpass
 import http.cookiejar
+
+import struct
 from lxml import html
 from tabulate import tabulate
+from Crypto.Cipher import AES
 
+base_url = 'http://localhost:8000'
+
+encrypt_file = False
+print("Welcome to SafeCollab File Download Application")
+choice = input("If you would like to encrypt a file, enter '1', otherwise to view reports, just hit 'enter': ")
+try:
+    if int(choice) == 1:
+        encrypt_file = True
+except ValueError:
+    pass
+
+if encrypt_file:
+    chunksize = 16 * 1024
+    path = input("Please enter full path of file to encrypt: ")
+    key = getpass.getpass(prompt="Please enter key for encrypted file: ")
+    f = open(path, 'rb')
+
+    iv = os.urandom(16)
+    encryptor = AES.new(key, AES.MODE_CBC, iv)
+    filesize = os.path.getsize(path)
+    with open(path, 'rb') as infile:
+        with open(path + '.enc', 'wb') as outfile:
+            outfile.write(struct.pack('<Q', filesize))
+            outfile.write(iv)
+            while True:
+                chunk = infile.read(chunksize)
+                if len(chunk) == 0:
+                    break
+                elif len(chunk) % 16 != 0:
+                    chunk += ' ' * (16 - len(chunk) % 16)
+                outfile.write(encryptor.encrypt(chunk))
+    quit()
 username = input("Please enter username: ")
 password = getpass.getpass(prompt="Please enter password: ")
 
@@ -36,7 +74,7 @@ login_page = opener.open('http://localhost:8000/myapplication/fdalogin/', data)
 #     f = open('error.html', 'wb')
 #     f.write(error.read())
 
-list = json.loads(opener.open('http://localhost:8000/myapplication/fdalistreports/').read().decode("utf-8"))
+list = json.loads(opener.open(base_url + '/myapplication/fdalistreports/').read().decode("utf-8"))
 
 if not list['success']:
     print("Login unsuccessful")
@@ -60,15 +98,16 @@ if report_num > len(data['num']):
     quit()
 
 try:
-    report_response = json.loads(opener.open('http://localhost:8000/myapplication/fdagetreport?reportID=' + str(data['ID'][report_num-1])).read().decode("utf-8"))
+    report_response = json.loads(opener.open(base_url + '/myapplication/fdagetreport?reportID=' + str(data['ID'][report_num-1])).read().decode("utf-8"))
 except urllib.request.HTTPError as error:
     f = open('error.html', 'wb')
     f.write(error.read())
 report = report_response['report']
-print("Description:", report['Description'])
-print("Author:", report['Author'])
-print("Date:", report['Date'])
-print("Content:", report['Content'])
+print("\n============== REPORT ==============\n")
+print("DESCRIPTION:", report['Description'])
+print("AUTHOR:", report['Author'])
+print("DATE:", report['Date'])
+print("CONTENT:", report['Content'])
 files_present = False
 if report_response['files']:
     files_present = True
@@ -76,14 +115,27 @@ if report_response['files']:
     i = 0
     for file in report_response['files']:
         i+=1
-        print("\t" + str(i) + ": " + file['name'])
+        print("\t" + str(i) + ": " + file['name'][2:])
 else:
     print("FILES: NONE")
 
 if files_present:
-    file_num = int(input("Enter number of file you would wish to download: "))
+    file_num = int(input("\nEnter number of file you would wish to download: "))
     file_path = input("Enter path where you would like to save the file: ")
-
-    with open(file_path + report_response['files'][file_num-1]['name'][2:], 'wb') as out_file:
-        download = opener.open('http://localhost:8000' + report_response['files'][file_num-1]['url']).read()
-        out_file.write(download)
+    if report['Encrypted'] == 0:
+        with open(file_path + report_response['files'][file_num-1]['name'][2:], 'wb') as out_file:
+            download = opener.open(base_url + report_response['files'][file_num-1]['url']).read()
+            out_file.write(download)
+    else:
+        chunksize = 16 * 1024
+        with open(file_path + report_response['files'][file_num-1]['name'][2:].replace(".enc", ""), 'wb') as out_file:
+            download = opener.open(base_url + report_response['files'][file_num-1]['url'])
+            key = getpass.getpass(prompt="Files are encrypted, please enter key: ")
+            origsize = struct.unpack('<Q', download.read(struct.calcsize('Q')))[0]
+            iv = download.read(16)
+            decryptor = AES.new(key, AES.MODE_CBC, iv)
+            while True:
+                chunk = download.read(chunksize)
+                if len(chunk) == 0:
+                    break
+                out_file.write(decryptor.decrypt(chunk))
