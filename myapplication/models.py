7dd5d9596cb5fc41e73218dc013d0a991ce8ae01
Daniel Codos <dbc5ba@virginia.edu>
2016-04-25 12:05:51 -0400

    Updated to remove auth token model

--- myapplication/models.py
+++ myapplication/models.py
@@ -1,7 +1,6 @@
 from django.conf import settings
 from django.db import models
 from django.contrib.auth.models import User
-from rest_framework.authtoken.models import Token
 
 
 
@@ -48,9 +47,3 @@ class ReportFolder(models.Model):
     owner = models.ForeignKey(User, null=True)
     name = models.CharField(max_length=50)
     reports = models.ManyToManyField(Report)
-
-
-@receiver(post_save, sender=settings.AUTH_USER_MODEL)
-def create_auth_token(sender, instance=None, created=False, **kwargs):
-    if created:
-        Token.objects.create(user=instance)
