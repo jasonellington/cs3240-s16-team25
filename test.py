8f625e3fdd541fa7d81ace4da183ca0c29d3f8ff
Daniel Codos <dbc5ba@virginia.edu>
2016-05-01 11:54:40 -0400

    Finished working FDA GUI (without hasing validation)

--- test.py
+++ test.py
@@ -1,13 +1,13 @@
 import json
+from tkinter import simpledialog
 from tkinter.ttk import Treeview
 
 from lxml import html
 import os
 import urllib.request
 from tkinter import *
-from tkinter.filedialog import askopenfilename
+from tkinter.filedialog import askopenfilename, asksaveasfilename
 import tkinter.messagebox as tm
-
 import struct
 from Crypto.Cipher import AES
 from Crypto.Hash import SHA256
@@ -117,7 +117,6 @@ class EncryptPage(Page):
         chunksize = 16 * 1024
         path = self.file_str.get()
         password = self.entry_pass.get()
-        f = open(path, 'rb')
         key = SHA256.new(password.encode('utf-8')).hexdigest()[:16]
         iv = os.urandom(16)
         encryptor = AES.new(key, AES.MODE_CBC, iv)
@@ -183,28 +182,43 @@ class ViewPage(Page):
         self.label_author = Label(self, text="Author:", font="bold")
         self.label_date = Label(self, text="Date:", font="bold")
         self.label_content = Label(self, text="Content:", font="bold")
+        self.label_files = Label(self, text="Files:", font="bold")
+        self.encrypted = False
 
         self.label_desc.grid(row=0, sticky=E)
         self.label_author.grid(row=1, sticky=E)
         self.label_date.grid(row=2, sticky=E)
         self.label_content.grid(row=3, sticky=E)
+        self.label_files.grid(row=4, sticky=E)
 
         self.report_desc = StringVar()
         self.report_author = StringVar()
         self.report_date = StringVar()
-        self.report_content = StringVar()
 
         self.val_desc = Label(self, textvariable=self.report_desc)
         self.val_author = Label(self, textvariable=self.report_author)
         self.val_date = Label(self, textvariable=self.report_date)
-        self.val_content = Label(self, textvariable=self.report_content)
+        self.val_content = Text(self, height=5)
+        self.file_tree = Treeview(self, height=5)
+        self.file_tree.bind("<Double-1>", self.onDoubleClick)
+        self.file_tree["show"] = "headings"
+        self.file_tree["columns"] = ("name", "encrypted", "url")
+        self.file_tree.column("name", width=200 )
+        self.file_tree.column("encrypted", width=25)
+        self.file_tree.column("url", width=10)
+        self.file_tree.heading("name", text="File Name")
+        self.file_tree.heading("encrypted", text="Encrypted")
+        self.file_tree.heading("url", text="URL")
+        self.file_tree["displaycolumns"] = ("name", "encrypted")
 
         self.val_desc.grid(row=0, column=2)
         self.val_author.grid(row=1, column=2)
         self.val_date.grid(row=2, column=2)
         self.val_content.grid(row=3, column=2)
+        self.file_tree.grid(row=4, column=2, sticky='sew')
 
-        self.pack()
+
+        # self.pack()
 
     def load_data(self, data, report_num):
         report_response = json.loads(opener.open(base_url + '/myapplication/fdagetreport?reportID=' + str(data['ID'][int(report_num)-1])).read().decode("utf-8"))
@@ -213,17 +227,78 @@ class ViewPage(Page):
         self.report_desc.set(report['Description'])
         self.report_author.set(report['Author'])
         self.report_date.set(report['Date'])
-        self.report_content.set(report['Content'])
+        self.val_content.insert(INSERT, report['Content'])
+        if report['Encrypted']:
+            self.encrypted = True
+        self.val_content.config(state=DISABLED)
         files_present = False
         if 'files' in report_response:
             files_present = True
-            print("FILES:")
             i = 0
             for file in report_response['files']:
                 i+=1
+                self.file_tree.insert("", i-1, str(i), values=(file['name'][2:], report['Encrypted'], file['url']))
                 print("\t" + str(i) + ": " + file['name'][2:])
         else:
-            print("FILES: NONE")
+            pass
+
+    def onDoubleClick(self, event):
+        item = self.file_tree.selection()[0]
+        file_name = self.file_tree.item(item, "values")[0]
+        file_url = self.file_tree.item(item, "values")[2]
+        extensions = file_name.split(".")
+        print(extensions)
+        if not self.encrypted:
+            ext = '.' + extensions[len(extensions)-1]
+            f = asksaveasfilename(title="Save File", defaultextension=ext, filetypes=[('Original File Type', ext)], initialfile=file_name)
+            with open(f, 'wb') as out_file:
+                download = opener.open(base_url + file_url).read()
+                out_file.write(download)
+                tm.showinfo("Success", "File successfully downloaded!")
+                reports_frame.lift()
+                reports_frame.load_data()
+        else:
+            ext = '.' + extensions[len(extensions)-2]
+            f = asksaveasfilename(title="Save File", defaultextension=ext, filetypes=[('Original File Type', ext)], initialfile=file_name.replace(".enc", ""))
+            decrypt_frame.lift()
+            decrypt_frame.decrypt(f, base_url + file_url)
+
+
+class DecryptPage(Page):
+    def __init__(self, *args, **kwargs):
+        Page.__init__(self, *args, **kwargs)
+        self.f = ""
+        self.url = ""
+        self.label_pass = Label(self, text="Enter password for decryption:")
+        self.entry_pass = Entry(self, show="*")
+        self.label_pass.grid(row=1, sticky=E)
+        self.entry_pass.grid(row=1, column=1)
+        self.decrypt_btn = Button(self, text="Decrypt File", command=self._decrypt_btn_clicked)
+        self.decrypt_btn.grid(columnspan=2)
+        self.pack()
+
+    def decrypt(self, f, url):
+        self.f = f
+        self.url = url
+
+
+    def _decrypt_btn_clicked(self):
+        chunksize = 16 * 1024
+        with open(self.f, 'wb') as out_file:
+            download = opener.open(self.url)
+            password = self.entry_pass.get()
+            key = SHA256.new(password.encode('utf-8')).hexdigest()[:16]
+            origsize = struct.unpack('<Q', download.read(struct.calcsize('Q')))[0]
+            iv = download.read(16)
+            decryptor = AES.new(key, AES.MODE_CBC, iv)
+            while True:
+                chunk = download.read(chunksize)
+                if len(chunk) == 0:
+                    break
+                out_file.write(decryptor.decrypt(chunk))
+            tm.showinfo("Success", "File successfully downloaded!")
+            reports_frame.lift()
+            reports_frame.load_data()
 
 
 class MainView(Frame):
@@ -234,15 +309,15 @@ class MainView(Frame):
         global encrypt_frame
         global reports_frame
         global view_frame
+        global decrypt_frame
         open_frame = OpenPage(self)
         login_frame = LoginPage(self)
         encrypt_frame = EncryptPage(self)
         reports_frame = ReportsPage(self)
         view_frame = ViewPage(self)
+        decrypt_frame = DecryptPage(self)
 
-        buttonframe = Frame(self)
         container = Frame(self)
-        buttonframe.pack(side="top", fill="x", expand=False)
         container.pack(side="top", fill="both", expand=True)
 
         open_frame.place(in_=container, x=0, y=0, relwidth=1, relheight=1)
@@ -250,6 +325,7 @@ class MainView(Frame):
         encrypt_frame.place(in_=container, x=0, y=0, relwidth=1, relheight=1)
         reports_frame.place(in_=container, x=0, y=0, relwidth=1, relheight=1)
         view_frame.place(in_=container, x=0, y=0, relwidth=1, relheight=1)
+        decrypt_frame.place(in_=container, x=0, y=0, relwidth=1, relheight=1)
 
         open_frame.show()
 
@@ -257,6 +333,6 @@ if __name__ == "__main__":
     root = Tk()
     main = MainView(root)
     main.pack(side="top", fill="both", expand=True)
-    root.wm_geometry("800x400")
+    root.wm_geometry("800x500")
     root.title('SafeCollab File Download Application')
     root.mainloop()
